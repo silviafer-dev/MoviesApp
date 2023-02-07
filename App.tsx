c6c96099477ca313f308a9c7cbf89afd3011e476
Silvia Ferrai <silviafer@hotmail.it>
2023-02-07 15:13:00 +0100

    Create gradient context

--- App.tsx
+++ App.tsx
@@ -2,13 +2,20 @@ import React from 'react';
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { Navigation } from './src/navigation/Navigation';
+import { GradientProvider } from 'src/context/GradientContext';
 // import { FadeScreen } from './src/screens/FadeScreen';
+
+const AppState = ({ children }: any) => {
+  return <GradientProvider>{children}</GradientProvider>;
+};
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Navigation />
-      {/* <FadeScreen /> */}
+      <AppState>
+        <Navigation />
+        {/* <FadeScreen /> */}
+      </AppState>
     </NavigationContainer>
   );
 };
---
+++ src/context/GradientContext.tsx
@@ -0,0 +1,40 @@
+import React, { createContext, useState } from 'react';
+
+interface ImageColors {
+  primary: string;
+  secondary: string;
+}
+
+interface ContextProps {
+  colors: ImageColors;
+  prevColors: ImageColors;
+  setMainColors: (colors: ImageColors) => void;
+  setPrevMainColors: (colors: ImageColors) => void;
+}
+
+export const GradientContext = createContext({} as ContextProps); //definir tipo
+
+export const GradientProvider = ({ children }: any) => {
+  const [colors, setColors] = useState<ImageColors>({
+    primary: 'transparent',
+    secondary: 'transparent',
+  });
+
+  const [prevColors, setPrevColors] = useState<ImageColors>({
+    primary: 'transparent',
+    secondary: 'transparent',
+  });
+  const setMainColors = (color: ImageColors) => {
+    setColors(color);
+  };
+  const setPrevMainColors = (color: ImageColors) => {
+    setPrevColors(color);
+  };
+
+  return (
+    <GradientContext.Provider
+      value={{ colors, prevColors, setMainColors, setPrevMainColors }}>
+      {children}
+    </GradientContext.Provider>
+  );
+};
